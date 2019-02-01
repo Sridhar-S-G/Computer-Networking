adf56fe8c1c74bcad74ab15d46cc5d35a780cc29
Sridhar-S-G <39511658+Sridhar-S-G@users.noreply.github.com>
2019-02-01 19:58:24 +0530

    Update BitStuffing_&_Destuffing.java

--- BitStuffing_&_Destuffing.java
+++ BitStuffing_&_Destuffing.java
@@ -11,26 +11,26 @@ public class  SGS
               for(int i=0;i<data.length();i++)
               {    if(data.charAt(i) == '1')
                         {counter++;
-                            Bits_stuff = Bits_stuff + data.charAt(i);}
-                   else {Bits_stuff = Bits_stuff + data.charAt(i);counter = 0;
+                            Bits_stuff += data.charAt(i);}
+                   else {Bits_stuff += data.charAt(i);counter = 0;
                         }
                    if(counter == 5)
-                        {Bits_stuff = Bits_stuff + '0';
+                        {Bits_stuff +=  '0';
                             counter = 0;}}
             System.out.println("Bit Stuffed Message " +Bits_stuff);
             counter=0;
             for(int i=0;i<Bits_stuff.length();i++)
                 {if(Bits_stuff.charAt(i) == '1')
                         {counter++;
-                            Bits_destuff = Bits_destuff + Bits_stuff.charAt(i);}
+                            Bits_destuff += Bits_stuff.charAt(i);}
                     else
-                        {Bits_destuff = Bits_destuff + Bits_stuff.charAt(i);
+                        {Bits_destuff += Bits_stuff.charAt(i);
                              counter = 0;}
                    if(counter == 5)
                         {if((i+2)!=Bits_stuff.length())
-                              Bits_destuff = Bits_destuff + Bits_stuff.charAt(i+2);
+                              Bits_destuff += Bits_stuff.charAt(i+2);
                               else
-                              Bits_destuff=Bits_destuff + '1';
+                              Bits_destuff+= '1';
                               i=i+2;
                               counter = 1;}}
                     System.out.println("The Destuffed Message : "+Bits_destuff);}}
